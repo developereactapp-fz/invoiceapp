960fcae280200d3500610530b1d8ca0e29ffde8c
SuitCostomize <developereactapp-fz>
2026-01-27 16:30:41 +0530

    Initial project setup

--- src/components/invoice/InvoicePreview.jsx
+++ src/components/invoice/InvoicePreview.jsx
@@ -19,15 +19,24 @@ export default function InvoicePreview() {
       <Box
         sx={{
           display: "flex",
-          justifyContent: "space-between",
+          justifyContent: "center",
           alignItems: "flex-start",
-          mb: 2,
+          mb: 7,
         }}
       >
         {/* LEFT TITLE (UNCHANGED) */}
         <Typography sx={{ fontWeight: 700, letterSpacing: 1 }}>
           TAX INVOICE
         </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "right",
+          alignItems: "flex-start",
+          mb: 5,
+        }}
+      >
 
         {/* RIGHT LOGO ONLY (NEW) */}
         <Box>
@@ -36,6 +45,17 @@ export default function InvoicePreview() {
             alt="Company Logo"
             style={{ height: 55, objectFit: "contain" }}
           />
+        </Box>
+      </Box>
+      {/* TOP INFO (UNCHANGED) */}
+      <Box sx={{ display: "flex", justifyContent: "right", mb: 2 }}>
+        <Box textAlign="right">
+          <Typography fontWeight={600}>HOLIDAYXTREMETOURISM LLC</Typography>
+          <Typography>Deira, Fijri Murar</Typography>
+          <Typography>Dubai</Typography>
+          <Typography>TRN: 104402382600003</Typography>
+          <Typography>Date: 17/01/2026</Typography>
+          <Typography>Invoice No: 38011</Typography>
         </Box>
       </Box>
 
@@ -47,15 +67,6 @@ export default function InvoicePreview() {
           <Typography>Adds: Mehra National Park</Typography>
           <Typography>UAE</Typography>
           <Typography>TRN: 100349964900003</Typography>
-        </Box>
-
-        <Box textAlign="right">
-          <Typography fontWeight={600}>HOLIDAYXTREMETOURISM LLC</Typography>
-          <Typography>Deira, Fijri Murar</Typography>
-          <Typography>Dubai</Typography>
-          <Typography>TRN: 104402382600003</Typography>
-          <Typography>Date: 17/01/2026</Typography>
-          <Typography>Invoice No: 38011</Typography>
         </Box>
       </Box>
 
@@ -115,11 +126,11 @@ export default function InvoicePreview() {
       <Divider sx={{ my: 2 }} />
 
       {/* FOOTER (UNCHANGED) */}
-      <Box sx={{ textAlign: "center", fontSize: 12 }}>
-        <Typography>
+      <Box sx={{ textAlign: "center", fontSize: 12 , marginTop: 20 }}>
+        <Typography sx={{fontSize: 15}}>
           Phone: +971507555393 | P.O. Box: 76823, UAE
         </Typography>
-        <Typography>
+        <Typography sx={{fontSize: 17}}>
           www.holidayxtreme.com | info@holidayxtreme.com
         </Typography>
       </Box>
