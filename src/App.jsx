630eeddc5f39f30f7564ed2b93a9ad184616682a
SuitCostomize <developereactapp-fz>
2026-01-28 13:11:15 +0530

    Initial project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
 
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Box } from "@mui/material";
+import Sidebar from "./layout/Sidebar";
 import InvoiceCreate from "./pages/InvoiceCreate";
-import MainLayout from "./layout/MainLayout";
 
 export default function App() {
   return (
-    <MainLayout>
-      
-      <InvoiceCreate />
-    </MainLayout>
-    
+    <BrowserRouter>
+        
+      <Box sx={{ display: "flex", minHeight: "100vh" }}>
+        {/* SIDEBAR */}
+        <Sidebar />
+
+        {/* MAIN CONTENT */}
+        <Box sx={{ flex: 1, p: 2 }}>
+          <Routes>
+            {/* Default Redirect */}
+            <Route path="/" element={<Navigate to="/invoice" replace />} />
+
+            {/* DOCUMENT PAGES */}
+            <Route
+              path="/invoice"
+              element={<InvoiceCreate type="invoice" />}
+            />
+            <Route
+              path="/receipt"
+              element={<InvoiceCreate type="receipt" />}
+            />
+            <Route
+              path="/quotation"
+              element={<InvoiceCreate type="quotation" />}
+            />
+          </Routes>
+        </Box>
+      </Box>
+    </BrowserRouter>
   );
 }
